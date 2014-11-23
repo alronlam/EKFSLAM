78839db92d1e0d98466419ced5f71507be6c6ff9
Alron <alronjan@yahoo.com>
2014-11-23 23:48:45 +0800

    Added Pure VINS IDP to the MainDriver.

--- src/driver/MainDriver.java
+++ src/driver/MainDriver.java
@@ -1,5 +1,7 @@
 package driver;
 
+import idp.VINSIDPController;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -24,11 +26,13 @@ public class MainDriver {
 		BreadcrumbController breadcrumb = new BreadcrumbController();
 		INSController ins = new INSController();
 		VINSController vins = new VINSController();
+		VINSIDPController vinsIDP = new VINSIDPController();
 
 		/* Initialize the logs for all three techniques */
 		StringBuilder breadcrumbLog = new StringBuilder();
 		StringBuilder insLog = new StringBuilder();
 		StringBuilder vinsLog = new StringBuilder();
+		StringBuilder vinsIDPLog = new StringBuilder();
 
 		/* Load IMU Dataset */
 		IMULogReader imuLogReader = new IMULogReader("data/imu");
@@ -39,6 +43,7 @@ public class MainDriver {
 		List<Mat> imgDataset = imgLogReader.readImages();
 
 		FeatureManager featureManager = new FeatureManager();
+		idp.features.FeatureManager featureManagerIDP = new idp.features.FeatureManager();
 
 		/* Make sure their sizes match */
 
@@ -51,22 +56,28 @@ public class MainDriver {
 			breadcrumb.predict(currIMUBatch);
 			ins.predict(currIMUBatch);
 			vins.predict(currIMUBatch);
+			vinsIDP.predict(currIMUBatch);
 
 			/* Image Update */
 			FeatureUpdate featureUpdate = featureManager.getFeatureUpdate(imgDataset.get(i));
 			breadcrumb.update(featureUpdate);
 			vins.update(featureUpdate);
 
+			idp.features.FeatureUpdate idpFeatureUpdate = featureManagerIDP.getFeatureUpdate(imgDataset.get(i));
+			vinsIDP.update(idpFeatureUpdate);
+
 			/* Update the logs per controller */
 			breadcrumbLog.append(breadcrumb.getDeviceCoords() + "\n");
 			insLog.append(ins.getDeviceCoords() + "\n");
 			vinsLog.append(vins.getDeviceCoords() + "\n");
+			vinsIDPLog.append(vinsIDP.getDeviceCoords() + "\n");
 		}
 
 		String folder = "results/";
 		writeToFile(folder + "breadcrumb.csv", breadcrumbLog.toString());
 		writeToFile(folder + "ins.csv", insLog.toString());
 		writeToFile(folder + "vins.csv", vinsLog.toString());
+		writeToFile(folder + "vinsidp.csv", vinsIDPLog.toString());
 	}
 
 	private static void writeToFile(String targetFilePath, String toWrite) {
