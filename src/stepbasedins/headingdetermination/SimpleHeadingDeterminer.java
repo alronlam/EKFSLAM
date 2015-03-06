e6e75ca466782647a60938a40ff76c436188ac4a
Alron <alronjan@yahoo.com>
2015-03-06 17:43:31 +0800

    Fixed SimpleHeadingDeterminer. It previously had the code for PerBatchesHeadingDeterminer.

--- src/stepbasedins/headingdetermination/SimpleHeadingDeterminer.java
+++ src/stepbasedins/headingdetermination/SimpleHeadingDeterminer.java
@@ -6,32 +6,10 @@ import stepbasedins.data.SensorEntry;
 
 public class SimpleHeadingDeterminer implements HeadingDeterminer {
 
-	private int batchCount;
-	private Double runningAvg;
-	private int windowSize = 3;
-	private ArrayList<Double> window;
-
-	public SimpleHeadingDeterminer() {
-		window = new ArrayList<Double>();
-	}
-
 	@Override
 	public double getHeading(ArrayList<SensorEntry> batch) {
 		double avg = HeadingDeterminerUtil.calculateAvgSensorEntry(batch);
-		window.add(avg);
-
-		if (window.size() < windowSize) {
-			runningAvg = HeadingDeterminerUtil.calculateAvgDouble(window);
-		} else {
-			runningAvg -= window.remove(0) / windowSize;
-			runningAvg += avg / windowSize;
-		}
-
-		return runningAvg;
-
-		// Log.d("Average angle is ", avg+"");
-		// return avg;
-		// return getNearestDiscreteHeading(avg);
+		return avg;
 	}
 
 }
