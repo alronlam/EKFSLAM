55718816b6ea620cc14f7bd34f904101b0cbf013
Alron <alronjan@yahoo.com>
2015-04-06 02:34:55 +0800

    Distance and heading error plotting functionality.

--- src/pathcomparison/CSVVectorComparatorDriver.java
+++ src/pathcomparison/CSVVectorComparatorDriver.java
@@ -5,12 +5,9 @@ import java.util.ArrayList;
 
 import util.FileLog;
 
-import commondata.PointDouble;
-
-public class CSVComparatorDriver {
+public class CSVVectorComparatorDriver {
 
 	public static void main(String[] args) {
-
 		// for each folder in the csvcompare folder,
 		// print folder name, and determine its correct dataset
 		// read all csv files:
@@ -31,20 +28,24 @@ public class CSVComparatorDriver {
 			for (File csvFile : csvFiles) {
 
 				System.out.println(csvFile.getName());
-				ArrayList<PointDouble> pathToCheck = CSVReader.readCSVPoints(csvFile);
-				ArrayList<PointDouble> correctPath = PathGenerator
-						.generate(datasetFolder.getName(), pathToCheck.size());
+				ArrayList<Vector> pathToCheck = PointListToVectorListConverter.convertPointList(CSVReader
+						.readCSVPoints(csvFile));
+				ArrayList<Vector> correctPath = PointListToVectorListConverter.convertPointList(PathGenerator.generate(
+						datasetFolder.getName(), pathToCheck.size()));
 
-				double avgPositionError = PathComparator.compare(pathToCheck, correctPath);
-				resultFile.append(csvFile.getName() + "(" + pathToCheck.size() + " steps): " + avgPositionError
-						+ "m\r\n");
+				ArrayList<Double> distanceErrorList = PathVectorListComparator.getDistanceErrorList(pathToCheck,
+						correctPath);
+				ArrayList<Double> headingErrorList = PathVectorListComparator.getHeadingErrorList(pathToCheck,
+						correctPath);
+
+				ComparisonResultWriter.writeToCSVFile(datasetFolder + "/result_errors/" + csvFile.getName(),
+						distanceErrorList, headingErrorList);
 
 			}
 
 			resultFile.writeToFile();
 
 		}
-
 	}
 
 }
---
+++ src/pathcomparison/PathVectorListComparator.java
@@ -0,0 +1,30 @@
+package pathcomparison;
+
+import java.util.ArrayList;
+
+public class PathVectorListComparator {
+
+	public static ArrayList<Double> getDistanceErrorList(ArrayList<Vector> actualList, ArrayList<Vector> correctList) {
+
+		ArrayList<Double> distanceErrorList = new ArrayList<Double>();
+
+		int limit = Math.min(actualList.size(), correctList.size());
+		for (int i = 0; i < limit; i++) {
+			distanceErrorList.add(actualList.get(i).getMagnitude() - correctList.get(i).getMagnitude());
+		}
+
+		return distanceErrorList;
+	}
+
+	public static ArrayList<Double> getHeadingErrorList(ArrayList<Vector> actualList, ArrayList<Vector> correctList) {
+
+		ArrayList<Double> headingErrorList = new ArrayList<Double>();
+
+		int limit = Math.min(actualList.size(), correctList.size());
+		for (int i = 0; i < limit; i++) {
+			headingErrorList.add(actualList.get(i).getDirection() - correctList.get(i).getDirection());
+		}
+
+		return headingErrorList;
+	}
+}
